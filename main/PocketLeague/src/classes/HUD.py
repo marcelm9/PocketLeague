0014b7cbaadbfe1001076d0d6914ec61d5f22339
Pascal Heinrich <pascalheinrich93@gmail.com>
2024-04-03 11:38:08 +0200

    added HUD

--- main/PocketLeague/src/classes/HUD.py
+++ main/PocketLeague/src/classes/HUD.py
@@ -1,15 +1,125 @@
 from .player import Player
+from ..files.config import *
+import pygame
+import PygameXtras as px
 
 class HUD:
 
-    def init(players: list[Player]):
-        pass
+    screen: pygame.Surface
+    labels = [] 
 
+    def init(screen):
+        assert len(Player.players) in (2,4)
+        HUD.screen = screen
+        team_color = [
+            TEAM0_COLOR,
+            TEAM1_COLOR,
+        ] 
+        if len(Player.players) == 2: 
+            HUD.labels.append(
+                px.Label(
+                    screen,
+                    Player.players[0].name,
+                    HUD_TEXT_SIZE,
+                    HUD_SIDE_DISTANCE,
+                    "topleft",
+                    f = HUD_FONT,
+                    bgc = Player.players[0].color,
+                    fd = HUD_DIMENSIONS,
+                    bc = team_color[Player.players[0].team],
+                    bw = HUD_BW,
+                    br = HUD_BR,
+                    to = HUD_TEXT_OFFSET
+                )
+            )
+            HUD.labels.append(
+                px.Label(
+                    screen,
+                    Player.players[1].name,
+                    HUD_TEXT_SIZE,
+                    (WIN_WIDTH - HUD_SIDE_DISTANCE[0], HUD_SIDE_DISTANCE[1]),
+                    "topright",
+                    f = HUD_FONT,
+                    bgc = Player.players[1].color,
+                    fd = HUD_DIMENSIONS,
+                    bc = team_color[Player.players[1].team],
+                    bw = HUD_BW,
+                    br = HUD_BR,
+                    to = HUD_TEXT_OFFSET
+                )
+            )
+        if len(Player.players) == 4: 
+            HUD.labels.append(
+                px.Label(
+                    screen,
+                    Player.players[0].name,
+                    HUD_TEXT_SIZE,
+                    HUD_SIDE_DISTANCE,
+                    "topleft",
+                    f = HUD_FONT,
+                    bgc = Player.players[0].color,
+                    fd = HUD_DIMENSIONS,
+                    bc = team_color[Player.players[0].team],
+                    bw = HUD_BW,
+                    br = HUD_BR,
+                    to = HUD_TEXT_OFFSET
+                )
+            )
+            HUD.labels.append(
+                px.Label(
+                    screen,
+                    Player.players[1].name,
+                    HUD_TEXT_SIZE,
+                    (HUD_SIDE_DISTANCE[0], WIN_HEIGHT - HUD_SIDE_DISTANCE[1]),
+                    "bottomleft",
+                    f = HUD_FONT,
+                    bgc = Player.players[1].color,
+                    fd = HUD_DIMENSIONS,
+                    bc = team_color[Player.players[1].team],
+                    bw = HUD_BW,
+                    br = HUD_BR,
+                    to = HUD_TEXT_OFFSET
+                )
+            )
+            HUD.labels.append(
+                px.Label(
+                    screen,
+                    Player.players[2].name,
+                    HUD_TEXT_SIZE,
+                    (WIN_WIDTH - HUD_SIDE_DISTANCE[0], HUD_SIDE_DISTANCE[1]),
+                    "topright",
+                    f = HUD_FONT,
+                    bgc = Player.players[2].color,
+                    fd = HUD_DIMENSIONS,
+                    bc = team_color[Player.players[2].team],
+                    bw = HUD_BW,
+                    br = HUD_BR,
+                    to = HUD_TEXT_OFFSET
+                )
+            )
+            HUD.labels.append(
+                px.Label(
+                    screen,
+                    Player.players[3].name,
+                    HUD_TEXT_SIZE,
+                    (WIN_WIDTH - HUD_SIDE_DISTANCE[0], WIN_HEIGHT -  HUD_SIDE_DISTANCE[1]),
+                    "bottomright",
+                    f = HUD_FONT,
+                    bgc = Player.players[3].color,
+                    fd = HUD_DIMENSIONS,
+                    bc = team_color[Player.players[3].team],
+                    bw = HUD_BW,
+                    br = HUD_BR,
+                    to = HUD_TEXT_OFFSET
+                )
+            )
+        
     def goal_team_1():
         pass
 
     def goal_team_2():
         pass
 
-    def draw(surface):
-        pass
+    def draw():
+        for label in HUD.labels:
+            label.draw()
--- main/PocketLeague/src/game.py
+++ main/PocketLeague/src/game.py
@@ -6,6 +6,7 @@ from .classes.renderer import Renderer
 from .classes.field import Field
 from .classes.ball_manager import BallManager
 from .classes.player import Player
+from .classes.HUD import HUD
 
 class Game:
 
@@ -31,22 +32,80 @@ class Game:
         
         Field.init()
         p = Player()
-        # p.set_keyboard_input(
-        #     up = pygame.K_w,
-        #     down = pygame.K_s,
-        #     left = pygame.K_a,
-        #     right = pygame.K_d,
-        #     dash = pygame.K_SPACE
-        # )
-        p.set_controller_input(
-            controller_index = 0,
-            joystick = 0,
-            dash_button = 9
-        )
+        p.set_keyboard_input(
+             up = pygame.K_w,
+             down = pygame.K_s,
+             left = pygame.K_a,
+             right = pygame.K_d,
+             dash = pygame.K_SPACE
+         )
+        #p.set_controller_input(
+         #   controller_index = 0,
+          #  joystick = 0,
+           # dash_button = 9
+        #)
         p.set_pos((300,300))
         p.set_name("Marcel")
         p.set_team(0)
         p.set_color((255,0,0))
+
+        
+        p2 = Player()
+        p2.set_keyboard_input(
+             up = pygame.K_w,
+             down = pygame.K_s,
+             left = pygame.K_a,
+             right = pygame.K_d,
+             dash = pygame.K_SPACE
+         )
+        #p.set_controller_input(
+         #   controller_index = 0,
+          #  joystick = 0,
+           # dash_button = 9
+        #)
+        p2.set_pos((400,400))
+        p2.set_name("PauliGOAT")
+        p2.set_team(0)
+        p2.set_color((255,0,255))
+
+        p3 = Player()
+        p3.set_keyboard_input(
+             up = pygame.K_w,
+             down = pygame.K_s,
+             left = pygame.K_a,
+             right = pygame.K_d,
+             dash = pygame.K_SPACE
+         )
+        #p.set_controller_input(
+         #   controller_index = 0,
+          #  joystick = 0,
+           # dash_button = 9
+        #)
+        p3.set_pos((400,400))
+        p3.set_name("Monke")
+        p3.set_team(1)
+        p3.set_color((20,255,0))
+
+        p4 = Player()
+        p4.set_keyboard_input(
+             up = pygame.K_w,
+             down = pygame.K_s,
+             left = pygame.K_a,
+             right = pygame.K_d,
+             dash = pygame.K_SPACE
+         )
+        #p.set_controller_input(
+         #   controller_index = 0,
+          #  joystick = 0,
+           # dash_button = 9
+        #)
+        p4.set_pos((400,400))
+        p4.set_name("Brrrrr")
+        p4.set_team(1)
+        p4.set_color((255,255,0))
+
+
+        HUD.init(Game.screen)
 
         BallManager.add_ball(
             CENTER, (0,0), BALL_RADIUS
